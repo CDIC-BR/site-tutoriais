d2e7215e32ca1a0ce79a65e0b56d1e5da6192f03
cdic <cdic-br@cnbb.org.br>
2025-08-25 10:50:16 -0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,13 +78,13 @@ const config = {
           },
 
           { href: 'https://forms.office.com/r/FPeGpNUy0k', label: 'Avalie-nos', position: 'left' },
-          /* { href: 'https://www.cnbb.org.br/', label: 'CNBB', position: 'left' },
+          { href: 'https://www.cnbb.org.br/', label: 'CNBB', position: 'left' },
           {
             href: 'https://www.edicoescnbb.com.br/',
             label: 'Edições CNBB',
-            position: 'left', */
-          }, 
-          🔹 Alterado para apontar para a nova página de contato
+            position: 'left',
+          },
+           🔹 Alterado para apontar para a nova página de contato
           { to: '/contato', label: 'Contate-nos', position: 'left' }, 
           {
             href: 'https://sistemacdicbr.cnbb.org.br/',
