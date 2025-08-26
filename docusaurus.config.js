c1a4af99218f3f2a5843f916768def776072bbcf
cdic <cdic-br@cnbb.org.br>
2025-08-26 08:48:56 -0300

    p

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,10 +21,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://cdic-br.github.io/site-tutoriais/',
+  url: 'https://cdic-br.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/site-tutoriais/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
