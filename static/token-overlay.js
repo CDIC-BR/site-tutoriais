5b372f5fdc4dcdf7a434172635eff407a7ac72cc
cdic <cdic-br@cnbb.org.br>
2025-09-02 17:14:40 -0300

    ,

--- static/token-overlay.js
+++ static/token-overlay.js
@@ -1,21 +1,20 @@
+// Executa imediatamente quando o HTML carrega
 (function() {
   try {
     var params = new URLSearchParams(window.location.search);
     var token = params.get('token');
     var ok = localStorage.getItem('cdic_token_ok') === '1';
 
-    if (!ok) {
-      if (token === 'CDIC2025') {
-        localStorage.setItem('cdic_token_ok', '1');
-        ok = true;
-        if (history.replaceState) {
-          history.replaceState({}, document.title, location.pathname + location.hash);
-        }
+    if (!ok && token === 'CDIC2025') {
+      localStorage.setItem('cdic_token_ok', '1');
+      ok = true;
+      if (history.replaceState) {
+        history.replaceState({}, document.title, location.pathname + location.hash);
       }
     }
 
     if (!ok) {
-      // cria overlay cobrindo tudo
+      // cria overlay cobrindo todo site
       var div = document.createElement('div');
       div.style.position = 'fixed';
       div.style.top = 0;
@@ -26,11 +25,11 @@
       div.style.display = 'flex';
       div.style.justifyContent = 'center';
       div.style.alignItems = 'center';
+      div.style.zIndex = 99999;
       div.style.fontFamily = 'sans-serif';
       div.style.textAlign = 'center';
-      div.style.zIndex = 99999;
       div.innerHTML = '<div><h1>Acesso não autorizado 🚫</h1><p>Você precisa acessar pelo sistema CDIC.</p></div>';
       document.documentElement.appendChild(div);
     }
-  } catch (e) {}
+  } catch(e) {}
 })();
